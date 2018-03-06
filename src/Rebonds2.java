a798b33ea8cbb861e5b418cb7828f46ca43ef8a3
ralima <ralima@PC707.alteca.fr>
2018-03-06 16:44:49 +0100

    MAJ Code

--- src/Rebonds2.java
+++ src/Rebonds2.java
@@ -3,39 +3,59 @@ import java.util.Scanner;
 public class Rebonds2 {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		//Constante de gravité
+		final double G = 9.81;
+		// Déclarations
+		// vitesses avant et après le rebond
+		double v = 0.0;
+		double v1 = 0.0;
+		// hauteur avant le rebond, hauteur de remontée
+		double h = 0.0;
+		double h1 = 0.0;
+		double h0 = 0.0;
+		double eps = 0.0;
+		double h_fin = 0.0;
 		
-		Scanner clavier = new Scanner(System.in);
-		
-		double g = 9.81, H0, eps, v, v1, h;
-		int nbr = 0;
-		
+		/*
+		 * Entrée des valeurs par l'utilisateur,
+		 * avec test de validité
+		 */
 		do {
-			System.out.print("Entrez la heuteur initiale H0 = ");
-			H0 = clavier.nextDouble();	
-		} while (H0 < 0);
-		
+			System.out.print("Coefficient de rebond (0 <= coeff < 1) : ");
+			eps = scanner.nextDouble();
+		} while ((eps < 0.0) || (eps >= 1.0));
 		do {
-			System.out.print("Entrez le coefficient de rebond (0 <= esp < 1) eps = ");
-			eps = clavier.nextDouble();	
-		} while (eps < 0 && eps >1);
-		
+			System.out.print("Hauteur initiale (0 <= H0) : ");
+			h0 = scanner.nextDouble();
+		} while (h0 < 0.0);
 		do {
-			System.out.print("Entrez la hauteur finale désirée (0 < h < H0) h = ");
-			h = clavier.nextDouble();	
-		} while (h < 0 && h > H0);
+			System.out.println("Hauteur finale (0 <= h_fin ) : ");
+			h_fin = scanner.nextDouble();
+		} while (h_fin < 0.0 || h_fin > h0);
 		
-		v = Math.sqrt(2*H0*g);
-		
+		// Boucle de calcul
+		h = h0;
+		int count = 0;
 		do {
-			v1 = eps*v;
-			H0 = v1*v1/(2*g);
-			v = Math.sqrt(2*H0*g);
-			nbr++;
-		} while (H0 > h);
+			v = Math.sqrt(2.0 * G * h);
+			// vitesse après le rebond
+			v1 = eps * v;
+			// incrémente le nombre de rebonds
+			count++;
+			// la hauteur à laquelle elle remonte...
+			h1 = (v1 * v1) / (2.0 * G);
+			// ...qui devient la nouvelle hauteur initiale
+			h = h1;
+			System.out.println("rebond " + count + " : " + h );
+		} while (h1 > h_fin);
 		
-		System.out.print("Le nombre de rebonds pour que la hauteur de la balle soit inférieure ou égale à " + h + " est " + nbr);
-		
-		clavier.close();
+		// Affichage du résultat
+		System.out.print("Le nombre de rebonds pour que la hauteur de la balle soit inférieure ou égale à " + h + " est " + count);
+
+		scanner.close();
 
 	}
 
